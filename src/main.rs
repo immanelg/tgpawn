4195c234699794715ada63617c13798c6d0fb0be
immanelg <119798691+immanelg@users.noreply.github.com>
2024-04-07 14:50:49 +0300

    refactor

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ use shakmaty::san::San;
 use shakmaty::uci::Uci;
 use shakmaty::{CastlingMode, Chess, Color, Move, Outcome, Position};
 use sqlx::sqlite::{Sqlite, SqlitePool};
-use sqlx::{Connection, Executor};
+use sqlx::{Connection, Executor, Pool};
+use std::error;
 use std::pin::pin;
 use std::{collections::HashMap, env};
 use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
@@ -23,6 +24,12 @@ enum Termination {
     Resign = 1,
     Checkmate = 2,
     Draw = 3,
+}
+
+struct State {
+    db: Pool<Sqlite>,
+    client: Client,
+    boards: HashMap<i64, Chess>,
 }
 
 fn packed_chat(id: i64) -> PackedChat {
@@ -45,7 +52,146 @@ fn parse_move(notation: &str, board: &impl Position) -> Option<Move> {
         .and_then(|uci| uci.to_move(board).ok())
 }
 
-async fn handle_update(client: &mut Client, db: sqlx::Pool<Sqlite>, update: Update) -> Result<()> {
+async fn on_start(state: &mut State, user_id: i64) -> Result<()> {
+    // TODO: accept initial position?
+    // TODO: ratings?
+    if maybe_playing_game.is_some() {
+        debug!("already in game {user_id}");
+        state.client
+            .send_message(packed_chat(user_id), "You are already playing. Type `resign` to leave.")
+            .await?;
+        return Ok(());
+    };
+
+    let maybe_pairable: Option<(i64, Option<i64>, Option<i64>)> = sqlx::query_as("select id, w_id, b_id from games where (b_id is null or w_id is null) and ended = 0 limit 1")
+        .fetch_optional(&state.db).await?;
+    debug!("maybe_pairable? {maybe_pairable:?}");
+
+    if let Some((id, w_id, b_id)) = maybe_pairable {
+        let (w_id, b_id) = match (w_id, b_id) {
+            (Some(w_id), None) => (w_id, user_id),
+            (None, Some(b_id)) => (user_id, b_id),
+            _ => {
+                panic!("oh how surprising! you are stupid! {maybe_pairable:?}")
+            }
+        };
+        let (_id, w_id, b_id) = sqlx::query_as::<_, (i64, i64, i64)>(
+            "update games set w_id = $1, b_id = $2 where games.id = $3 returning id, w_id, b_id"
+        )
+            .bind(w_id)
+            .bind(b_id)
+            .bind(id)
+            .fetch_one(&state.db)
+        .await?;
+        let (white, black) = (packed_chat(w_id), packed_chat(b_id));
+        state.client
+            .send_message(white, "You are white. Your turn!")
+        .await?;
+        state.client
+            .send_message(black, "You are black. Waiting for opponent's move.")
+        .await?;
+    } else {
+        let (id,) = sqlx::query_as::<_, (i64,)>("insert into games (w_id, b_id, winner, ended, fen) values ($1, null, null, 0, $2) returning id").bind(user_id).bind(STARTING_FEN).fetch_one(&state.db).await?;
+        debug!("create new game {id}");
+        state.client
+            .send_message(
+                packed_chat(user_id),
+                "Created a new game. Waiting for an opponent to join.",
+            )
+            .await?;
+    }
+    Ok(())
+}
+
+async fn on_move(state: &mut State,user_id: i64, notation: &str ) -> Result<()> {
+    let Some((id, w_id, b_id, _winner, _termination, fen)) = maybe_playing_game
+    else {
+        state.client
+            .send_message(packed_chat(user_id), "Type `start` to join a game")
+            .await?;
+        return Ok(());
+    };
+    let board = state.boards.entry(id).or_insert_with(|| {
+        fen.parse::<Fen>()
+            .expect("fen from db")
+            .into_position(CastlingMode::Standard)
+            .expect("valid initial position")
+    });
+    if !(board.turn() == Color::White && user_id == w_id || board.turn() == Color::Black && user_id == b_id) {
+        state.client.send_message(packed_chat(user_id), "Not your turn!").await?;
+        return Ok(());
+    }
+    let Some(m) = parse_move(notation, board) else {
+        state.client
+            .send_message(packed_chat(user_id), "This is not a valid move")
+        .await?;
+        return Ok(());
+    };
+    if !board.is_legal(&m) {
+        state.client.send_message(packed_chat(user_id), "This move is not legal").await?;
+        return Ok(());
+    }
+    board.play_unchecked(&m); 
+    debug!("playing move {m}");
+
+    let ended = board.is_game_over();
+    let fen =
+    Fen::from_position(board.clone(), shakmaty::EnPassantMode::Always)
+        .to_string();
+    let winner = if ended {
+        Some(if board.turn().is_white() { w_id } else { b_id })
+    } else {
+        None
+    };
+    let termination = board.outcome().and_then(|o| match o {
+        Outcome::Draw => None,
+        Outcome::Decisive {
+            winner: Color::Black,
+        } => Some(true),
+        Outcome::Decisive {
+            winner: Color::White,
+        } => Some(false),
+    });
+
+    sqlx::query(
+        "insert into moves (game_id, ply, uci) values ($1, (select count(*) from moves where game_id = $1), $2)"
+    )
+        .bind(id)
+        .bind(m.to_uci(CastlingMode::Standard).to_string())
+        .execute(&state.db).await?;
+
+    sqlx::query(
+        "update games set ended = $1, winner = $2, termination = $3, fen = $4 where id = $1")
+        .bind(ended)
+        .bind(winner)
+        .bind(termination)
+        .bind(&fen)
+        .bind(id)
+        .execute(&state.db).await?;
+
+    for &c in [packed_chat(w_id), packed_chat(b_id)].iter() {
+        // show fen image
+        state.client.send_message(packed_chat(user_id), format!("Played {m}, FEN is now {fen}")).await?;
+        if ended {
+            state.client.send_message(packed_chat(user_id), format!("Game is over")).await?;
+        }
+    }
+    if ended {
+        state.boards.remove(&id);
+    }
+    Ok(())
+}
+
+async fn on_resign(state: &mut State, user_id: i64) -> Result<()> {
+    if let Some(_) = maybe_playing_game {
+        error!("todo: resign");
+    } else {
+        error!("reject: need to join a game");
+    }
+    Ok(())
+}
+
+async fn handle_update(state: &mut State, update: Update) -> Result<()> {
     match update {
         Update::NewMessage(message) if !message.outgoing() => {
             let chat = message.chat();
@@ -53,164 +199,32 @@ async fn handle_update(client: &mut Client, db: sqlx::Pool<Sqlite>, update: Upda
             let user_name = chat.name();
             let text = message.text();
 
-            let c = packed_chat(user_id);
+            // let c = packed_chat(user_id);
 
             info!("message by {user_id} {user_name}: {text}");
 
             sqlx::query("insert or ignore into users (id) values ($1)")
                 .bind(user_id)
-                .execute(&db)
+                .execute(&state.db)
             .await?;
 
             debug!("insert user {user_id}");
 
             let maybe_playing_game: Option<(i64, i64, i64, bool, i64, String)> = sqlx::query_as(
                 "select id, w_id, b_id, winner, termination, fen from games where (w_id = $1 or b_id = $1) and ended = 0",
-            ).bind(user_id).fetch_optional(&db).await?;
+            ).bind(user_id).fetch_optional(&state.db).await?;
 
             debug!("get ongoing game for {user_id}: got {maybe_playing_game:?}");
 
             match text.as_ref() {
-                "nuke" => {
-                    sqlx::query("delete from games").execute(&db).await.unwrap();
-                    sqlx::query("delete from users").execute(&db).await.unwrap();
-                    sqlx::query("delete from moves").execute(&db).await.unwrap();
-                }
                 "start" => {
-                    // TODO: accept initial position?
-                    // TODO: ratings?
-                    if maybe_playing_game.is_some() {
-                        debug!("already in game {user_id}");
-                        client
-                            .send_message(c, "You are already playing. Type `resign` to leave.")
-                            .await
-                            .unwrap();
-                        return;
-                    };
-
-                    let maybe_pairable: Option<(i64, Option<i64>, Option<i64>)> = sqlx::query_as("select id, w_id, b_id from games where (b_id is null or w_id is null) and ended = 0 limit 1")
-                        .fetch_optional(&db).await.unwrap();
-                    debug!("maybe_pairable? {maybe_pairable:?}");
-
-                    if let Some((id, w_id, b_id)) = maybe_pairable {
-                        let (w_id, b_id) = match (w_id, b_id) {
-                            (Some(w_id), None) => (w_id, user_id),
-                            (None, Some(b_id)) => (user_id, b_id),
-                            _ => {
-                                panic!("oh how surprising! you are stupid! {maybe_pairable:?}")
-                            }
-                        };
-                        let (_id, w_id, b_id) = sqlx::query_as::<_, (i64, i64, i64)>(
-                            "update games set w_id = $1, b_id = $2 where games.id = $3 returning id, w_id, b_id"
-                        )
-                            .bind(w_id)
-                            .bind(b_id)
-                            .bind(id)
-                            .fetch_one(&db)
-                        .await?;
-                        let (white, black) = (packed_chat(w_id), packed_chat(b_id));
-                        client
-                            .send_message(white, "You are white. Your turn!")
-                        .await?;
-                        client
-                            .send_message(black, "You are black. Waiting for opponent's move.")
-                        .await?;
-                    } else {
-                        let (id,) = sqlx::query_as::<_, (i64,)>("insert into games (w_id, b_id, winner, ended, fen) values ($1, null, null, 0, $2) returning id").bind(user_id).bind(STARTING_FEN).fetch_one(&db).await?;
-                        debug!("create new game {id}");
-                        client
-                            .send_message(
-                                c,
-                                "Created a new game. Waiting for an opponent to join.",
-                            )
-                            .await
-                            .unwrap();
-                    }
+                    on_start(state, user_id).await?;
                 }
                 "resign" => {
-                    if let Some(_) = maybe_playing_game {
-                        todo!("resign");
-                    } else {
-                        todo!("reject: need to join a game");
-                    }
+                    on_resign(state, user_id).await?;
                 }
                 notation => {
-                    let Some((id, w_id, b_id, _winner, _termination, fen)) = maybe_playing_game
-                    else {
-                        client
-                            .send_message(c, "Type `start` to join a game")
-                            .await
-                            .unwrap();
-                        return;
-                    };
-                    let board = boards.entry(id).or_insert_with(|| {
-                        fen.parse::<Fen>()
-                            .expect("fen from db")
-                            .into_position(CastlingMode::Standard)
-                            .expect("valid initial position")
-                    });
-                    if !(board.turn() == Color::White && user_id == w_id || board.turn() == Color::Black && user_id == b_id) {
-                        client.send_message(chat, "Not your turn!").await?;
-                        return;
-                    }
-                    let Some(m) = parse_move(notation, board) else {
-                        client
-                            .send_message(chat, "This is not a valid move")
-                        .await?;
-                        return;
-                    };
-                    if !board.is_legal(&m) {
-                        client.send_message(chat, "This move is not legal").await?;
-                        return;
-                    }
-                    board.play_unchecked(&m);
-                    debug!("playing move {m}");
-
-                    let ended = board.is_game_over();
-                    let fen =
-                    Fen::from_position(board.clone(), shakmaty::EnPassantMode::Always)
-                        .to_string();
-                    let winner = if ended {
-                        Some(if board.turn().is_white() { w_id } else { b_id })
-                    } else {
-                        None
-                    };
-                    let termination = board.outcome().and_then(|o| match o {
-                        Outcome::Draw => None,
-                        Outcome::Decisive {
-                            winner: Color::Black,
-                        } => Some(true),
-                        Outcome::Decisive {
-                            winner: Color::White,
-                        } => Some(false),
-                    });
-
-                    sqlx::query(
-                        "insert into moves (game_id, ply, uci) values ($1, (select count(*) from moves where game_id = $1), $2)"
-                    )
-                        .bind(id)
-                        .bind(m.to_uci(CastlingMode::Standard).to_string())
-                        .execute(&db).await?;
-
-                    sqlx::query(
-                        "update games set ended = $1, winner = $2, termination = $3, fen = $4 where id = $1")
-                        .bind(ended)
-                        .bind(winner)
-                        .bind(termination)
-                        .bind(&fen)
-                        .bind(id)
-                        .execute(&db).await?;
-
-                    for &c in [packed_chat(w_id), packed_chat(b_id)].iter() {
-                        // show fen image
-                        client.send_message(c, format!("Played {m}, FEN is now {fen}")).await?;
-                        if ended {
-                            client.send_message(c, format!("Game is over")).await?;
-                        }
-                    }
-                    if ended {
-                        boards.remove(&id);
-                    }
+                    on_move(state, user_id, notation ).await?;
                 }
             }
         }
@@ -220,6 +234,7 @@ async fn handle_update(client: &mut Client, db: sqlx::Pool<Sqlite>, update: Upda
     }
     Ok(())
 }
+
 
 async fn async_main() -> Result<()> {
     env_logger::init();
@@ -257,11 +272,16 @@ async fn async_main() -> Result<()> {
     .await?;
 
     if !client.is_authorized().await? {
-        info!("Signing in...");
         client.bot_sign_in(&token).await?;
         client.session().save_to_file(SESSION_FILE)?;
-        info!("Signed in!");
+        info!("signed in");
     }
+
+    let mut state = State {
+        client,
+        db,
+        boards,
+    };
 
     info!("waiting for messages");
 
@@ -274,14 +294,19 @@ async fn async_main() -> Result<()> {
             }
         };
         match update {
-            Some(update) => handle_update(&mut client, db, update),
+            Some(update) => {
+                if let Err(e) = handle_update(&mut state, update).await {
+                    error!("error while handling update {e}");
+                }
+            },
             None => break,
         }
-        
+
     }
 
-    info!("Saving session file and exiting...");
-    client.session().save_to_file(SESSION_FILE)?;
+    info!("exiting");
+    state.client.session().save_to_file(SESSION_FILE)?;
+
     Ok(())
 }
 
